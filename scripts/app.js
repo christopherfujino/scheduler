ed0d0cf5baaa0119ec05b107c9ef5bd7be7bbcec
Christopher Fujino <christopherfujino@gmail.com>
2015-04-01 19:47:04 -1000

    get rid of task.endTime property, always just create new Date() object

--- scripts/app.js
+++ scripts/app.js
@@ -30,7 +30,7 @@ app.controller('main', ['$scope', '$timeout', function($scope, $timeout) {
 
   function Task(arg, startNow) { // 2nd arg is bool
     console.log(startNow);
-    this.initTime = this.endTime = new Date(); // initialize both times to now
+    this.initTime = new Date();
     if(typeof arg === 'string') { // create new task with arg being name
       this.name = arg; // arg coming from text input box
       this.cumulativeTime = 0; // time lapsed prior to a pause, integer
@@ -53,9 +53,13 @@ app.controller('main', ['$scope', '$timeout', function($scope, $timeout) {
     this.toggleName = function() { if (this.nameBool) this.nameBool = false; else this.nameBool = true; }
 
     function increment(task) {
-      task.endTime = new Date();
+      task.timeoutHandler = $timeout(increment, 1000, true, task);  // immediately restart timer
+      var now = new Date();
       task.setLapsedTime();
-      task.timeoutHandler = $timeout(increment, 1000, true, task);
+      console.log(now - task.initTime);
+      if((now - task.initTime) > 30000) { // save every 30 seconds
+
+      }
     }
 
     this.setLapsedTime = function() {
@@ -81,13 +85,15 @@ app.controller('main', ['$scope', '$timeout', function($scope, $timeout) {
         this.timeoutHandler = $timeout(increment, 1000, true, this); // 3rd arg is def, 4th is arg to pass to increment
         this.isPaused = false;
       }
-      else {
-        this.cumulativeTime += new Date().getTime() - this.initTime.getTime();
+      else {  // task still running, let's pause
         $timeout.cancel(this.timeoutHandler);
         this.isPaused = true;
-
-        $scope.updateStorage();
+        this.save();
       }
+    };
+    this.save = function() { //
+      this.cumulativeTime += new Date().getTime() - this.initTime.getTime();
+      $scope.updateStorage();
     };
     this.remove = function(index) {
       $scope.taskStore.splice(index,1);
