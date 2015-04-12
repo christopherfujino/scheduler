169f2f746519ba2c06b5c09fd790243e6400fc08
Christopher Fujino <christopherfujino@gmail.com>
2015-04-11 21:16:58 -1000

    add logic for updating tags.time and removing tags

--- scripts/app.js
+++ scripts/app.js
@@ -3,19 +3,43 @@
 var app = angular.module('scheduler',[]); // requires angularJS version >= 1.4
 
 app.controller('main', ['$scope', '$timeout', function($scope, $timeout) {
-  $scope.version = '0.9a';
+  $scope.version = '0.9';
   $scope.nameBool = false; // name is clicked?
 
   $scope.taskStore = {
     version : $scope.version,
     activeTasks : [],
     toDoTasks : [],
-    tags : []
+    tags : [] // of the form { 'tag' : 'my tag name', 'time' : 5500 }
   };
 
-  var timeString = function(x) {
+  var tagRefreshHandler = null;
+
+  function tagRefresh() {
+    $timeout.cancel(tagRefreshHandler); // cancel any other scheduled refresh
+    tagRefreshHandler = $timeout(tagRefresh, 60000);  // schedule another refresh
+
+    for(var k=0; k<$scope.taskStore.tags.length; k++) { // iterate through each tag of taskStore.tags[]
+      var query = $scope.taskStore.tags[k];
+
+      // for queryTag, sum up all cumulativeTime of tasks with this tag
+      var ms = 0,
+        active = $scope.taskStore.activeTasks;
+      for(var i=0; i<active.length; i++) { // iterate through all active tasks
+        for(var j=0; j<active[i].tags.length; j++) { // iterate through all tags of each task
+          if(active[i].tags[j] === query.tag) {
+            ms += active[i].cumulativeTime;
+            break;
+          }
+        }
+      }
+      query.time = $scope.timeString(ms);
+    }
+  }
+
+  $scope.timeString = function(x) {
     /*
-      this takes a quantity of seconds x, and converts it to a string of the
+      this takes a quantity of milliseconds x, and converts it to a string of the
       form Hs:Ms:Ss
     */
     x = Math.floor(x/1000);
@@ -64,10 +88,24 @@ app.controller('main', ['$scope', '$timeout', function($scope, $timeout) {
       if(temp.error !== true) $scope.taskStore.toDoTasks.push(temp); // don't push if Task() returns error object
     });
     if (obj.tags) { // if tags[] exists from memory, copy it into $scope.taskStore.tags[]
-      $scope.taskStore.tags = obj.tags.slice(0);
+      if (typeof obj.tags[0] === 'string') { // api <= 0.9a
+        obj.tags.forEach(function(tag){
+          $scope.taskStore.tags.push({
+            'tag' : tag,
+            'time' : null
+          });
+        });
+      }
+      else if (typeof obj.tags[0] === 'object') {
+        $scope.taskStore.tags = obj.tags.slice(0);
+      }
+      else {
+        throw new Error('Huh?!');
+      }
     }
   }
-  console.log($scope.taskStore);
+//  console.log($scope.taskStore);
+  tagRefresh();  // refresh tag times
 
   $scope.updateStorage = function() {
     console.log('local storage updated!');
@@ -91,15 +129,51 @@ app.controller('main', ['$scope', '$timeout', function($scope, $timeout) {
     if(task.addTag(tag)) {
       var dupe = false; // is this tag already listed in taskStore.tags[]?
       $scope.taskStore.tags.forEach(function(x){
-        if(x===tag) dupe=true;
+        if(x.tag===tag) dupe=true;
       });
-      if(!dupe) $scope.taskStore.tags.push(tag); // only add this tag if it's unique
+      if(!dupe) {
+        $scope.taskStore.tags.push({'tag': tag, 'time': null}); // only add this tag if it's unique
+        tagRefresh();
+      }
       $scope.updateStorage();
     };
   };
 
   $scope.removeTag = function(tag, task) {
     if(task.removeTag(tag)){
+      var unique = true, // assume this tag was unique, unless another is found
+        loopDone = false; // should we break the outer loop?
+      // check if tags should be removed, implement
+      for(var i=0; !loopDone && i<$scope.taskStore.activeTasks.length; i++) {
+        var task = $scope.taskStore.activeTasks[i];
+        for(var j=0; j<task.tags.length; j++) {
+          if (task.tags[j].tag === tag) {
+            unique = false;
+            loopDone = true; // done looping, tag not unique
+            break;
+          }
+        }
+      }
+
+      for(var i=0; !loopDone && i<$scope.taskStore.toDoTasks.length; i++) {
+        var task = $scope.taskStore.toDoTasks[i];
+        for(var j=0; j<task.tags.length; j++) {
+          if (task.tags[j].tag === tag) {
+            unique = false;
+            loopDone = true; // done looping, tag not unique
+            break;
+          }
+        }
+      }
+      if (unique) { // tag was unique, remove it from $scope.taskStore.tags[]
+        for(var i=0; i<$scope.taskStore.tags.length; i++) {
+          if(tag === $scope.taskStore.tags[i].tag) {
+            $scope.taskStore.tags.splice(i, 1);
+            console.log('tag ' + tag + ' deleted!');
+          }
+        }
+      }
+//      else { console.log('not so unique.'); }
       $scope.updateStorage();
     }
   }
@@ -145,7 +219,7 @@ app.controller('main', ['$scope', '$timeout', function($scope, $timeout) {
     }
 
     this.setLapsedTime = function() {
-      this.lapsedTimeString = timeString(this.cumulativeTime + new Date().getTime() - this.initTime.getTime());
+      this.lapsedTimeString = $scope.timeString(this.cumulativeTime + new Date().getTime() - this.initTime.getTime());
     }; this.setLapsedTime(); // and now immediately call this function to initialize lapsedTimeString
 
     this.pause = function() {
